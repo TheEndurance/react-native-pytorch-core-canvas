66e918379c78eccd71f2a021b27dc0ff2915b0d6
TheEndurance <rawa.jalal@gmail.com>
2023-10-09 10:47:01 -0400

    comment out jsi

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { NativeModules, Platform } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'react-native-pytorch-core-canvas' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+// const LINKING_ERROR =
+//   `The package 'react-native-pytorch-core-canvas' doesn't seem to be linked. Make sure: \n\n` +
+//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+//   '- You rebuilt the app after installing the package\n' +
+//   '- You are not using Expo Go\n';
 
 // const PytorchCoreCanvas = NativeModules.PyTorchCoreJSI
 //   ? NativeModules.PyTorchCoreJSI
